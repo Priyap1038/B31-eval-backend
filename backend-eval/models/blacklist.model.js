2e9d13853d31d974d39ad3398d22d5fcdd9929ca
Priyap1038 <124154881+Priyap1038@users.noreply.github.com>
2023-12-11 16:12:46 +0530

    .

--- backend-eval/models/blacklist.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const blacklistSchema = mongoose.Schema({
-    blacklist: {type : [String]}
-});
-
-const BlacklistModel = mongoose.model("blacklist", blacklistSchema);
-
-module.exports = { BlacklistModel}
